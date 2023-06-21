b32d4b3edb150ae775447e5d2d36a2775fbcbe37
Basavaraja V <32273986+royaldevops@users.noreply.github.com>
2023-06-21 22:35:14 +0530

    Add files via upload

---
+++ magicapp.py
@@ -0,0 +1,12 @@
+import magic
+
+import magic
+
+def analyze_file(file_path):
+    m = magic.Magic()
+    file_type = m.from_file(file_path)
+    print(f"File: {file_path}")
+    print(f"Type: {file_type}")
+
+file_path = "description.txt"
+analyze_file(file_path)
